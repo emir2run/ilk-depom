6b78521cea82ae876d9080fcc33047f98f034d64
emir2run <152778062+emir2run@users.noreply.github.com>
2023-12-03 13:35:18 +0300

    Create ilk_depom.py

---
+++ ilk_depom.py
@@ -0,0 +1,8 @@
+sozluk = {"LOL": "komik bir şeye verilen cevap",
+          "CRINGE": "garip ya da utandırıcı bir şey",
+          "ROFL": "bir şakaya karşılık cevap",
+          "SHEESH": "onaylamamak",
+          "CREEPY": "korkunç",
+          "AGGRO": "agresifleşmek/sinirlenmek"}
+          
+print(sozluk["CREEPY"])
